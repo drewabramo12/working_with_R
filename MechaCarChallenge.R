a3c0a81dac29424f23ab35dd0676cf2df34837f0
Andrew Abramo <drewabramo12@gmail.com>
2022-04-17 23:51:58 -0700

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+# Deliverable 1
+
 library(dplyr)
 MechaCar_table <- read.csv(file='Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
@@ -6,3 +8,24 @@ Mecha_lm <- lm(mpg~vehicle_length +vehicle_weight + spoiler_angle + ground_clear
 
 #Summary
 summary(Mecha_lm)
+
+# Deliverable 2
+SuspensionCoil_table <- read.csv(file='Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Create Summary
+total_summary <- SuspensionCoil_table%>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Std_Dev=sd(PSI))
+
+# Deliverable 3
+
+
+t.test(SuspensionCoil_table$PSI, mu=1500)
+
+
+#Lot 1
+t.test(subset(suspension_data, Manufacturing_Lot="Lot1")$PSI, mu=1500)
+
+#Lot 2
+t.test(subset(suspension_data, Manufacturing_Lot="Lot2")$PSI, mu=1500)
+
+#Lot 3
+t.test(subset(suspension_data, Manufacturing_Lot="Lot3")$PSI, mu=1500)
